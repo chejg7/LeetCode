cd73138a8c665252e2148788ff55510370888dc5
Alex <70855337+chejg7@users.noreply.github.com>
2025-04-17 16:28:53 +0800

    Update 0121-best-time-to-buy-and-sell-stock-04-17-2025-16-26-56.py

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock-04-17-2025-16-26-56.py
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock-04-17-2025-16-26-56.py
@@ -10,3 +10,13 @@ class Solution:
                 max_profit = max(max_profit, price - min_price)
 
         return max_profit
+
+# 최초에 작성했던 코드는 시간 효율성이 O(n2)으로 실패함. 이 문제를 효율적으로 풀기 위해서는 각 요소를 한번씩만 확인하면서 최솟값을 기준으로 최대의 차이를 가지는 값만 찾아내면 됨. 
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         max_profits = []
+#         for i in range(len(prices)):
+#             max_profits.append(max(prices[i:]) - prices[i])  
+
+#         return max(max(max_profits), 0)
