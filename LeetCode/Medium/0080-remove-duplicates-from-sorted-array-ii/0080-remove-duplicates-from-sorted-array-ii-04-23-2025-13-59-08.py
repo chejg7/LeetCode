1872cea580c7955e279b0a34ac5c757b4f4432b1
Alex <70855337+chejg7@users.noreply.github.com>
2025-04-23 14:05:35 +0800

    Update 0080-remove-duplicates-from-sorted-array-ii-04-23-2025-13-59-08.py

--- LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii-04-23-2025-13-59-08.py
+++ LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii-04-23-2025-13-59-08.py
@@ -13,4 +13,17 @@ class Solution:
                 k = [nums[i], 1]
                 i += 1
         return i
-            
+
+        # 다른 풀이 : 값과 횟수를 저장하지 않고, 두 칸 앞과 다른 값이 나올 때 복사하는 방식으로 해결
+        # if len(nums) <= 2:
+        #     return len(nums)
+
+        # insert_pos = 2  # 세 번째 자리부터 검사 시작
+
+        # for i in range(2, len(nums)):
+        #     # 현재 값이 두 칸 앞의 값과 같지 않다면 복사
+        #     if nums[i] != nums[insert_pos - 2]:
+        #         nums[insert_pos] = nums[i]
+        #         insert_pos += 1
+
+        # return insert_pos
