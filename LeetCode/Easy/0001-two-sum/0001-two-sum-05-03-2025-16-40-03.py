cc735e88eac10b8f34ab914e93f406344a362df3
Alex <70855337+chejg7@users.noreply.github.com>
2025-05-03 16:42:44 +0800

    Update 0001-two-sum-05-03-2025-16-40-03.py

--- LeetCode/Easy/0001-two-sum/0001-two-sum-05-03-2025-16-40-03.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum-05-03-2025-16-40-03.py
@@ -4,3 +4,14 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+# 아래의 코드는 시간 복잡도가 O(n)
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         num_map = {}  # 숫자 → 인덱스
+#         for i, num in enumerate(nums):
+#             complement = target - num
+#             if complement in num_map:
+#                 return [num_map[complement], i]
+#             num_map[num] = i
