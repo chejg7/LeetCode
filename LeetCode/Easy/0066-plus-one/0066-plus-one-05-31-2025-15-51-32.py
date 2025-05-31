a079319d3969f4d1eb361fbd8bde9c7044334976
Alex <70855337+chejg7@users.noreply.github.com>
2025-05-31 15:54:13 +0800

    Update 0066-plus-one-05-31-2025-15-51-32.py

--- LeetCode/Easy/0066-plus-one/0066-plus-one-05-31-2025-15-51-32.py
+++ LeetCode/Easy/0066-plus-one/0066-plus-one-05-31-2025-15-51-32.py
@@ -3,7 +3,6 @@
 #         num = int(''.join([str(i) for i in digits])) + 1
 #         return [int(s) for s in list(str(num))]
 
-
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         for i in range(len(digits) - 1, -1, -1):
@@ -12,3 +11,8 @@ class Solution:
                 return digits
             digits[i] = 0
         return [1] + digits
+
+# 뒤에서부터 숫자를 올림 처리
+# 9일 경우 0으로 바꾸고 자리 올림
+# 전부 9일 경우 [1, 0, 0, ..., 0] 반환
+# ⏱ 시간 복잡도는 O(n) 이고, 문자열 변환 없이 처리 가능하므로 더 최적화된 방법입니다.
