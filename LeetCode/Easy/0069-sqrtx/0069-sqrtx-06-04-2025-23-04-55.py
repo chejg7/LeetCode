62f271c61dbab3c0b37d1f4fa69be164f70bc149
Alex <70855337+chejg7@users.noreply.github.com>
2025-06-04 23:08:05 +0800

    Update 0069-sqrtx-06-04-2025-23-04-55.py

--- LeetCode/Easy/0069-sqrtx/0069-sqrtx-06-04-2025-23-04-55.py
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx-06-04-2025-23-04-55.py
@@ -1,9 +1,25 @@
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         if x < 2:
+#             return x
+#         for i in range(1, x // 2 + 2):
+#             if i * i == x:
+#                 return i
+#             if i * i > x:
+#                 return i - 1
+
+
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
-        for i in range(1, x // 2 + 2):
-            if i * i == x:
-                return i
-            if i * i > x:
-                return i - 1
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
