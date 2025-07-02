5cf7d4a96b5cfe948ebf2e8e5b687c159bf5ff10
Alex <70855337+chejg7@users.noreply.github.com>
2025-07-02 21:33:56 +0800

    Update 0136-single-number-07-02-2025-21-29-15.py

--- LeetCode/Easy/0136-single-number/0136-single-number-07-02-2025-21-29-15.py
+++ LeetCode/Easy/0136-single-number/0136-single-number-07-02-2025-21-29-15.py
@@ -1,9 +1,19 @@
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         result = {}
+#         for num in nums:
+#             if num in result:
+#                 del result[num]
+#             else:
+#                 result[num] = 1
+#         return next(iter(result))
+
+# XOR 비트 연산을 사용하면 아주 쉽게 해결할 수 있음. 
+# XOR 자기 자신을 하면 0이 됨. 0과 자신을 XOR 연산하면 자기 자신이 됨. 
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = {}
+        result = 0
         for num in nums:
-            if num in result:
-                del result[num]
-            else:
-                result[num] = 1
-        return next(iter(result))
+            result ^= num  # XOR 연산
+        return result
